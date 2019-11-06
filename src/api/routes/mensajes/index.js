9db87990f5cc9cceac26eff1a2cb4d5f68237337
Ivan Guerrero <guerrero.ivan.789@gmai.com>
2019-11-06 18:06:54 -0500

    0.11.6.19.6.6.1

--- src/api/routes/mensajes/index.js
+++ src/api/routes/mensajes/index.js
@@ -48,17 +48,12 @@ router.post('/addFriend', /*token,*/ function(req, res, next){
             return res.send({success:false, msg:'Error 1'});
         }else{ 
             if(rows){
-                var data1 = await User.findOne({username: req.body.username},function(err,data1){
-                    
-                });
-                var user1 = await Friendlist.findOne({username: req.body.username}, function(err,user1){
-
-                });
+                var data1 = await User.findOne({username: req.body.username}).exec();
+                var user1 = await Friendlist.findOne({username: req.body.username}).exec();
                 console.log(data1);
                 user1.friends.push({username: data1.username, avatar: data1.avatar, nombresyapellidos: data1.nombresyapellidos});
                 user1.save();
-
-                var data2 = User.findOne({username: req.body.friend});
+                var data2 = User.findOne({username: req.body.friend}).exec();
                 var user2 = await Friendlist.findOne({username: req.body.friend})
                 user2.friends.push({username: data2.username, avatar: data2.avatar, nombresyapellidos: data2.nombresyapellidos});
                 user2.save();
