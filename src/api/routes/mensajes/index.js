9ba5db5a4d37c72afb1b2b58d5073efaa88a2a95
Ivan Guerrero <guerrero.ivan.789@gmai.com>
2019-11-06 18:56:35 -0500

    0.11.6.19.6.56.1

--- src/api/routes/mensajes/index.js
+++ src/api/routes/mensajes/index.js
@@ -133,16 +133,14 @@ router.post('/removeFriend', /*token,*/ async function(req, res, next){
                     return res.send({success:false, msg:'Error!'});
                 }else if(friendlist){
                     var iDelete = findIndexByUsername(friendlist.friends, req.body.username);
-                    friendlist.friends[iDelete] = undefined;
-                    console.log(iDelete)
+                    delete friendlist.friends[iDelete];
                     friendlist.save();
                     Friendlist.findOne({username:req.body.username}, function(err3,friendlist2){
                         if(err3){
                             return res.send({success:false, msg:'Error!'});
                         }else if(friendlist2){
                             var iDelete = findIndexByUsername(friendlist2.friends, user.username);
-                            console.log(iDelete)
-                            friendlist2.friends[iDelete] = undefined;
+                            delete friendlist2.friends[iDelete];
                             friendlist2.save();
                             return res.send({success:true, msg:'Eliminado!'});
                         }
